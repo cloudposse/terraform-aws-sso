14ec9845218fb9057a38a47e92918775095b80ca
Andriy Knysh <aknysh@users.noreply.github.com>
2021-06-04 19:32:56 -0400

    Update modules/account-assignments/variables.tf
    Co-authored-by: Johannes Grumböck <johannes.grumboeck@redbull.com>

--- modules/account-assignments/variables.tf
+++ modules/account-assignments/variables.tf
@@ -1,9 +1,9 @@
 variable "account_assignments" {
   type = list(object({
-    account            = string
+    account             = string
     permission_set_name = string
-    permission_set_arn = string
-    principal_name     = string
-    principal_type     = string
+    permission_set_arn  = string
+    principal_name      = string
+    principal_type      = string
   }))
 }
