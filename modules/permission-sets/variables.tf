0482c21cd4fe80155de641ad22d56d865d7958d4
Jeremy White <jeremy.white@cloudposse.com>
2023-11-01 17:42:47 -0400

    fix(permission-sets/policy-attachments): disallow empty paths

--- modules/permission-sets/variables.tf
+++ modules/permission-sets/variables.tf
@@ -9,9 +9,13 @@ variable "permission_sets" {
     policy_attachments = list(string)
     customer_managed_policy_attachments = list(object({
       name = string
-      path = string
+      path = optional(string, "/")
     }))
   }))
 
   default = []
+  validation {
+    error_message = "Customer managed policy attachment path cannot be empty"
+    condition     = !anytrue([for ps in var.permission_sets : anytrue([for p in ps.customer_managed_policy_attachments : p.path == ""])])
+  }
 }
